b8ebc285e076c3c653eba08a560b8cae4f74cd17
Ariel Bandeira <arieltwain@gmail.com>
2019-11-02 20:53:11 -0300

    initial commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Search from '../search/Search';
+import Results from '../results/Results';
 
 function App() {
   return (
@@ -18,15 +19,9 @@ function App() {
             </div>
             <Search />
           </div>
+          <Results />
           <span className="info">ex. Alien, Guardians Of The Galaxy, O Auto da Compadecida</span>
         </form>
-      </div>
-      <div className="s130">
-        <img src="../public/logo192.png" alt="img" /><br /></div>
-      <div className="s130">
-        <h3>Movie Info:  </h3><br/>
-        <h4>Title:  </h4><h5> </h5><br />
-        <h4>Year:  </h4><h5> </h5><br />
       </div>
     </div>
   );
---
+++ src/components/results/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+    render() {
+        return (
+            <div>
+                <img src="../public/logo192.png" alt="img" /><br />
+                <h3>Movie Info:  </h3><br />
+                <h4>Title:  </h4><h5> </h5><br />
+                <h4>Year:  </h4><h5> </h5><br />
+            </div>
+        );
+    }
+}
