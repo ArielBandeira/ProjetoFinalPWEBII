9212a9de3c96e349075f64f40da7911f7321cbea
Ariel Bandeira <arieltwain@gmail.com>
2019-11-02 23:05:31 -0300

    initial commit

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -4,24 +4,28 @@ export default class Search extends Component {
     render() {
         return (
             <div>
-                <img src="../public/logo192.png" alt="img" />
-                <h3>Movie Info:  </h3>
-                <h4>Title:  </h4><h5> </h5>
-                <h4>Year:  </h4><h5> </h5>
-                <h4>Rated:  </h4><h5> </h5>
-                <h4>Released:  </h4><h5> </h5>
-                <h4>Runtime:  </h4><h5> </h5>
-                <h4>Genre:  </h4><h5> </h5>
-                <h4>Director:  </h4><h5> </h5>
-                <h4>Writer:  </h4><h5> </h5>
-                <h4>Actors:  </h4><h5> </h5>
-                <h4>Plot:  </h4><h5> </h5>
-                <h4>Language:  </h4><h5> </h5>
-                <h4>Country:  </h4><h5> </h5>
-                <h4>Awards: </h4><h5> </h5>
-                <h4>Type:  </h4><h5> </h5>
-                <h4>BoxOffice:  </h4><h5> </h5>
-                <h4>Production:  </h4><h5> </h5>
+                <div>
+                    <img src="" alt="img" />
+                    <h3>Movie Info:  </h3>
+                    <h4>Title:  </h4><h5> </h5>
+                    <h4>Year:  </h4><h5> </h5>
+                    <h4>Rated:  </h4><h5> </h5>
+                    <h4>Released:  </h4><h5> </h5>
+                    <h4>Runtime:  </h4><h5> </h5>
+                    <h4>Genre:  </h4><h5> </h5>
+                    <h4>Director:  </h4><h5> </h5>
+                </div>
+                <div>
+                    <h4>Writer:  </h4><h5> </h5>
+                    <h4>Actors:  </h4><h5> </h5>
+                    <h4>Plot:  </h4><h5> </h5>
+                    <h4>Language:  </h4><h5> </h5>
+                    <h4>Country:  </h4><h5> </h5>
+                    <h4>Awards: </h4><h5> </h5>
+                    <h4>Type:  </h4><h5> </h5>
+                    <h4>BoxOffice:  </h4><h5> </h5>
+                    <h4>Production:  </h4><h5> </h5>
+                </div>
             </div>
         );
     }
